4b6f2620819815aa471ca82a8a707b8d7b1d1163
wladimir <wl.cordova@duocuc.cl>
2024-09-26 23:54:54 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'patitas',
+  webDir: 'www'
+};
+
+export default config;
