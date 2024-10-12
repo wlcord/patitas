f8b41bfec3fc57798390c181b47636d4fdec73cd
wladimir <wl.cordova@duocuc.cl>
2024-10-11 23:20:14 -0300

    Se cambio el mapbox por google maps

--- src/app/mapa/tab3.page.ts
+++ src/app/mapa/tab3.page.ts
@@ -1,29 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-import * as mapboxgl from 'mapbox-gl';
+import { GoogleMap } from '@capacitor-community/google-maps';
+import { Geolocation } from '@capacitor/geolocation';
 
+declare var google: any;
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page implements OnInit {
-
-  map!: mapboxgl.Map;
+  map: any;
+  userLocation: any;
 
   constructor() {}
 
-  ngOnInit(): void {
-    this.buildMap();
+  ngOnInit() {
+    this.loadMap();
   }
 
-  buildMap() {
-    (mapboxgl as any).accessToken = 'pk.eyJ1Ijoid2xhZGltaXIxIiwiYSI6ImNtMWZ2ZGEzdTI0cm8ycW9vNThiNWE1azQifQ.7XnLJQ8419e_U--c3oEm5g';
-    this.map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-74.5, 40], // Coordenadas iniciales [longitud, latitud]
-      zoom: 9 // Nivel de zoom inicial
+  async loadMap() {
+    // Obtener la geolocalización del dispositivo
+    const position = await Geolocation.getCurrentPosition({
+      enableHighAccuracy: true // Alta precisión
     });
-  }
+    
+    const myLatLng = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    };
 
+    // Inicializar el mapa
+    const mapEle = document.getElementById('map');
+    if (mapEle) {
+      this.map = new google.maps.Map(mapEle, {
+        center: myLatLng,
+        zoom: 15 // Aumentamos el zoom para ver mejor la ubicación actual
+      });
+
+      // Agregar marcador en la ubicación del usuario
+      const marker = new google.maps.Marker({
+        position: myLatLng,
+        map: this.map,
+        title: 'Mi ubicación actual'
+      });
+    }
+  }
+  
 }
